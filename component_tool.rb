9ef5f6cf86b6a67dbf99e6cde753dd60795403bc
Gábor Bottyán <gabor.bottyan@accedo.tv>
2019-12-05 23:17:05 +0100

    moved binary to github

--- component_tool.rb
+++ component_tool.rb
@@ -4,7 +4,7 @@
 class ComponentTool < Formula
   desc ""
   homepage ""
-  url "https://doc-10-9c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/93v2p24sj1tn15dkk597djmbrhldtpg0/1575036000000/02217535804365211572/*/1pM5V-pSpO4dpmXrn21fSgEaQ9s_IHE-h"
+  url "https://raw.githubusercontent.com/gaborbottyan/component-releases/master/comps.1.0.3.tar.gz"
   sha256 "991b2fbe6e74cfab5fd5d614c6cec4bf7194ed6b21e38e582fa4c714c82f8c80"
 
   # depends_on "cmake" => :build
