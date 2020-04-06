2cdc66eb9d0f7472bae86da9d0fe8814111f37fb
Gábor Bottyán <gabor.bottyan@accedo.tv>
2020-04-06 13:25:59 +0200

    version 1.0,7

--- component_tool.rb
+++ component_tool.rb
@@ -4,9 +4,9 @@
 class ComponentTool < Formula
   desc ""
   homepage ""
-  url "https://raw.githubusercontent.com/gaborbottyan/component-releases/master/comps.1.0.6.tar.gz"
-  sha256 "45691a0af3c4cc51017f749e0fb8572dc0778cf5068f3100acef2d351bce712b"
-  version "2.3"
+  url "https://raw.githubusercontent.com/gaborbottyan/component-releases/master/comps.1.0.7.tar.gz"
+  sha256 "b85d317f88f9cc9b5f9a5f0837f41b5b803b4024181327c58ae3285783c31a0a"
+  version "2.4"
   # depends_on "cmake" => :build
 
   depends_on "xcodegen" => :install
